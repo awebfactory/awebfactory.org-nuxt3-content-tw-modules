02d40b178e963281c38b322c3b91cdd5160f0787
Victor Kane <victorkane@gmail.com>
2022-10-27 16:45:00 -0300

    build(config): defineNuxtConfig has auto-import

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import defaultTheme from "tailwindcss/defaultTheme"
+import tailwindTypography from "@tailwindcss/typography"
+
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: defaultTheme.colors.green,
+      },
+    },
+  },
+}
