7d6e9a6a35faf6b76909f88ccf5cc7f53c8e17ba
Victor Kane <victorkane@gmail.com>
2022-10-28 07:46:27 -0300

    build(head element): initial SEO info added including dynamic title and favicon, other assets

--- app.vue
+++ app.vue
@@ -1,3 +1,25 @@
+<script setup>
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  charset: "utf-8",
+  title: "Project Flow & Tracker",
+  titleTemplate: (title) =>
+    title !== "Project Flow & Tracker"
+      ? `${title} · Project Flow & Tracker`
+      : title,
+  meta: [{ name: "description", content: "build in public, learn in public" }],
+  link: [
+    {
+      rel: "icon",
+      type: "image/ico",
+      href: "/favicon.ico",
+    },
+  ],
+})
+</script>
+
 <template>
   <div
     class="prose bg-texas-rose-100 p-12 w-full h-full min-h-screen flex flex-col items-center"
