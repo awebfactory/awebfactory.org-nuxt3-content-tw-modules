a7b962e7807a08cb928059a089a1449fe2f3fc84
Victor Kane <victorkane@gmail.com>
2022-10-28 06:45:19 -0300

    style(tailwind): added custom colors to theme settings

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="prose p-12 w-full h-full min-h-screen flex flex-col items-center">
+  <div
+    class="prose bg-texas-rose-100 p-12 w-full h-full min-h-screen flex flex-col items-center"
+  >
     <NuxtPage />
   </div>
 </template>
