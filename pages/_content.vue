b94ac8ea280e3d1f1dce99aebe13db00bc83203b
Victor Kane <victorkane@gmail.com>
2022-10-31 12:02:59 -0300

    build(content model): Rustic content model as a data dump at route _content

---
+++ pages/_content.vue
@@ -0,0 +1,15 @@
+<script setup>
+const { data } = await useAsyncData(() => {
+  // return queryContent().findOne()
+  const theContent = queryContent().find()
+  return theContent
+})
+// console.log(data)
+</script>
+
+<template>
+  <main>
+    <h2 class="text-awf-dark-brown">Content Model</h2>
+    <pre>{{ data }}</pre>
+  </main>
+</template>
