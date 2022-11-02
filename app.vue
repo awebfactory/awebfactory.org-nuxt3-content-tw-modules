98fb0b71ba36bfd641d43adf975597f30d2e1d96
Victor Kane <victorkane@gmail.com>
2022-11-02 10:06:48 -0300

    style(app): remove all styling from app view

--- app.vue
+++ app.vue
@@ -22,10 +22,6 @@ useHead({
 
 <template>
   <NuxtLayout>
-    <div
-      class="prose bg-texas-rose-100 p-12 w-full h-full min-h-screen flex flex-col items-center"
-    >
-      <NuxtPage />
-    </div>
+    <NuxtPage />
   </NuxtLayout>
 </template>
