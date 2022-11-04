4f66dc2b720ca99f2395c3a5535e48061455b71f
Victor Kane <victorkane@gmail.com>
2022-11-04 10:53:07 +0000

    build(catch-all): add logic for unfound docs as projects

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -2,8 +2,26 @@
   <main>
     <ContentDoc>
       <template #not-found>        
-        <pre>Document not found</pre>
+        <div class="p-4 mx-auto max-w-screen-md">
+        <p>
+          Hi there! Did you mean project{{" "}}{{ route. params.slug }}?
+          <span class="text-green-400">
+            <p>no doc</p>
+          </span>
+        </p>
+        <p>
+          Sorry, we can't find any project with that name. But, click to see a{{" "}}
+          <a href="project" class="bg-yellow-100 hover:bg-yellow-200">
+            recent project
+          </a>
+        </p>
+      </div>
       </template>
     </ContentDoc>
   </main>
 </template>
+
+<script setup>
+  const route = useRoute()
+  // console.log('hola', route.params) 
+</script>
