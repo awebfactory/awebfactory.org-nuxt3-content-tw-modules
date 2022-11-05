df18e3fd23890bd9650e78218af153d7a0d0a815
Victor Kane <victorkane@gmail.com>
2022-11-05 07:07:03 -0300

    fix(links): always use NuxtLink for internal links

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,27 +1,28 @@
 <template>
   <main>
     <ContentDoc>
-      <template #not-found>        
+      <template #not-found>
         <div class="p-4 mx-auto max-w-screen-md">
-        <p>
-          Hi there! Did you mean project{{" "}}{{ route. params.slug }}?
-          <span class="text-green-400">
-            <p>no doc</p>
-          </span>
-        </p>
-        <p>
-          Sorry, we can't find any project with that name. But, click to see a{{" "}}
-          <a href="project" class="bg-yellow-100 hover:bg-yellow-200">
-            recent project
-          </a>
-        </p>
-      </div>
+          <p>
+            Hi there! Did you mean project{{ " " }}{{ route.params.slug }}?
+            <span class="text-green-400">
+              <p>no doc</p>
+            </span>
+          </p>
+          <p>
+            Sorry, we can't find any project with that name. But, click to see
+            a{{ " " }}
+            <NuxtLink to="project" class="bg-yellow-100 hover:bg-yellow-200">
+              recent project
+            </NuxtLink>
+          </p>
+        </div>
       </template>
     </ContentDoc>
   </main>
 </template>
 
 <script setup>
-  const route = useRoute()
-  // console.log('hola', route.params) 
+const route = useRoute()
+// console.log('hola', route.params)
 </script>
