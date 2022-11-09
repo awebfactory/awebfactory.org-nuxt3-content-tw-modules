fba674fb70ea8b32c2a0c2f137b5f80572685a4d
Victor Kane <victorkane@gmail.com>
2022-11-09 08:01:08 -0300

    build(projects): implement initial individual project page (messy pedagogical version)

--- pages/projects/[project].vue
+++ pages/projects/[project].vue
@@ -1,5 +1,6 @@
 <template>
   <main>
+    <h1>Two ways to do things (pedagogical mess!)</h1>
     <ContentQuery :path="$route.path" v-slot="{ data }">
       <h2>{{ data[0].title }}</h2>
       <h3>{{ data[0].description }}</h3>
@@ -14,9 +15,27 @@
       </details>
     </ContentQuery>
     <ContentDoc />
+    <hr />
+    <hr />
+    <hr />
+    <hr />
+    <ContentRenderer :value="data">
+      <h2>{{ data.title }}</h2>
+      <h3>{{ data.description }}</h3>
+      <div>
+        <p>Start date: {{ data.startDate }}</p>
+        <p>End date: {{ data.endDate }}</p>
+      </div>
+      <!-- Content Query + ContentRendererMarkdown is the same as ContentDoc -->
+      <ContentRendererMarkdown :value="data" />
+      <ContentDoc />
+    </ContentRenderer>
   </main>
 </template>
 
 <script setup>
 const route = useRoute()
+const { data } = await useAsyncData("page-data", () =>
+  queryContent(route.path).findOne()
+)
 </script>
