28fa810da43258145349e06b6a0f912d077ba70a
Victor Kane <victorkane@gmail.com>
2022-11-09 07:27:05 -0300

    build(projects): implement initial individual project page

---
+++ pages/projects/[project].vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <ContentQuery :path="$route.path" v-slot="{ data }">
+      <h2>{{ data[0].title }}</h2>
+      <h3>{{ data[0].description }}</h3>
+      <div>
+        <p>Start date: {{ data[0].startDate }}</p>
+        <p>End date: {{ data[0].endDate }}</p>
+      </div>
+      <details>
+        <summary>Click for detailed project data</summary>
+        <pre>{{ data[0] }}</pre>
+        <ContentRenderer :value="data[0].excerpt" />
+      </details>
+    </ContentQuery>
+    <ContentDoc />
+  </main>
+</template>
+
+<script setup>
+const route = useRoute()
+</script>
